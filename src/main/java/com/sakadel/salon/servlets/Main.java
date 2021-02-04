49a47b0780ae7da0340bffbd2810e1ced18af74f
Dennis Sakadel <denyssakadel@gmail.com>
2021-02-04 19:28:49 +0200

    updated servlet

--- src/main/java/com/sakadel/salon/servlets/Main.java
+++ src/main/java/com/sakadel/salon/servlets/Main.java
@@ -1,5 +1,11 @@
 package com.sakadel.salon.servlets;
 
+import com.sakadel.salon.commands.CommandManager;
+import com.sakadel.salon.commands.ServletCommand;
+import org.apache.log4j.Logger;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,16 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Main extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //super.doGet(req, resp);
-        //PrintWriter printWriter = req.
-        //req.getRequestDispatcher("/index.jsp").forward(req,resp);
-        resp.getWriter().write("ez");
+
+    private static final Logger LOGGER = Logger.getLogger(Servlet.class);
+
+    private CommandManager commandManager;
+
+    public void init(ServletConfig config) throws ServletException {
+        LOGGER.info("Initializing Servlet");
+        commandManager = new CommandManager();
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        LOGGER.info("Processing get request");
+
+        ServletCommand command = commandManager.getGetCommand(request);
+        String page = command.execute(request, response);
+        request.getRequestDispatcher(page).forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        LOGGER.info("Processing post request");
+
+        ServletCommand command = commandManager.getPostCommand(request);
+        String page = command.execute(request, response);
+        request.getRequestDispatcher(page).forward(request, response);
     }
 }
